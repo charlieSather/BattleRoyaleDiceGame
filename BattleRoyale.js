2a45876ba8e246a5a75a08fe16e2a7a20a588919
Charlie Sather <charliesather18@gmail.com>
2020-02-25 11:07:09 -0600

    Board setup is now complete with 10 players present at beginning of game

--- BattleRoyale.js
+++ BattleRoyale.js
@@ -1,2 +1,53 @@
 "use strict"
 
+const numPlayers = 10;
+var players = [];
+var rounds = 6;
+var currentRound = 1;
+var dice = [4,6,8,10,12,20]
+
+function setupPlayers(){
+    for(let i = 1; i <= numPlayers; i++){
+        players.push({
+            Name: "Player " + i , 
+            RoundScore: 0
+        });
+    }
+}
+
+setupPlayers();
+console.log(players);
+
+var i = () => {
+        for(let i = 0; i < 100; i ++){
+            console.log(getRandom(4));
+    }
+}
+
+i();
+
+
+function rollDiceSet(player){
+    let sum = 0;
+    player
+    players.forEach(function (value,index, array){
+        sum += rollDice()
+    })
+}
+
+
+function rollDice(numSides){
+   return getRandom(numSides)
+}
+
+function getRandom(max){
+    return Math.floor(Math.random() * max) + 1; 
+}
+
+function playRound(){
+
+}
+
+function diceShootOut(){
+
+}
